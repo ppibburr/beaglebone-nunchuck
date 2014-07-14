1fcf083ec57e99ecf96901b20f273b00ca3426c1
ppibburr <ppibburr@ppibburr-Inspiron-N5050.(none)>
2014-07-13 23:35:29 -0400

    ...

--- sample/remote_mouse.rb
+++ sample/remote_mouse.rb
@@ -1,13 +1,27 @@
 require "nunchuck/xinput/mouse"
 require "xinput/remote"
 
-port = ARGV[0]
-
-unless port
-  raise "Must pass 'port' value: ie, /dev/i2c-1"
+def usage()
+  puts "ruby -I./lib ./sample/remote_mouse.rb XXX.XXX.X.X /dev/i2c-N"
 end
 
-mouse    = XInput::RemoteMouse.new ARGV[0]
+
+host = ARGV[0]
+port = ARGV[1]
+
+unless host
+  puts "Must pass 'host' value: ie, 127.0.0.1\n"
+  usage()
+  exit(127)
+end
+
+unless port
+  puts "Must pass 'port' value: ie, /dev/i2c-1\n"
+  usage()
+  exit(127)
+end
+
+mouse    = XInput::RemoteMouse.new host
 nunchuck = Nunchuck::Mouse.new(port, mouse)
 
 nunchuck.init()
